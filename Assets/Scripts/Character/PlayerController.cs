73addf6549e4f7d3f44611c60d03ca5ed2aa03d6
Emil Bureaca <emilovici.17@gmail.com>
2019-12-26 19:45:07 +0200

    Added idle, move, walk, and run animations.

---
+++ Assets/Scripts/Character/PlayerController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 2;
+    public float runSpeed = 6;
+    public float gravity = -12;
+    public float jumpHeight = 1;
+    [Range(0, 1)]
+    public float airControlPercent;
+
+    public float pushForce = 3.0f;
+
+    public float turnSmoothTime = 0.2f;
+    float turnSmoothVelocity;
+
+    public float speedSmoothTime = 0.1f;
+    float speedSmoothVelocity;
+    float currentSpeed;
+    float velocityY;
+
+    Animator animator;
+    Transform cameraT;
+    CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        cameraT = Camera.main.transform;
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = input.normalized;
+        bool running = Input.GetKey(KeyCode.LeftShift);
+
+        Move(inputDir, running);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        // animator
+        float animationSpeedPercent = ((running) ? currentSpeed / runSpeed : currentSpeed / walkSpeed * .5f);
+        animator.SetFloat("speedMove", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+    }
+
+    void Move(Vector2 inputDir, bool running)
+    {
+        if (inputDir != Vector2.zero)
+        {
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, GetModifiedSmoothTime(turnSmoothTime));
+        }
+
+        float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, GetModifiedSmoothTime(speedSmoothTime));
+
+        velocityY += Time.deltaTime * gravity;
+        Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
+
+        controller.Move(velocity * Time.deltaTime);
+        currentSpeed = new Vector2(controller.velocity.x, controller.velocity.z).magnitude;
+
+        if (controller.isGrounded)
+        {
+            velocityY = 0;
+            animator.SetBool("isJumping", false);
+        }
+
+    }
+
+    void Jump()
+    {
+        if (controller.isGrounded)
+        {
+            float jumpVelocity = Mathf.Sqrt(-2 * gravity * jumpHeight * (currentSpeed / 20 + 5f));
+            velocityY = jumpVelocity;
+            animator.SetTrigger("jumpTrigger");
+            animator.SetBool("isJumping", true);
+        }
+    }
+
+    float GetModifiedSmoothTime(float smoothTime)
+    {
+        if (controller.isGrounded)
+        {
+            return smoothTime;
+        }
+
+        if (airControlPercent == 0)
+        {
+            return float.MaxValue;
+        }
+        return smoothTime / airControlPercent;
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (body != null && !body.isKinematic)
+        {
+            body.velocity = hit.moveDirection * pushForce;
+        }
+    }
+}
